ddc0d7573b62dc5c41eae571166c1b0cd2b7865e
Alexa Herrera <alexa.herrera@digitaloperative.com>
2016-09-09 12:24:57 -0700

    Fix like everything.

--- bot.py
+++ bot.py
@@ -5,6 +5,7 @@ import random
 import threading
 import time
 import sys
+import string
 
 client = discord.Client()
 
@@ -23,9 +24,23 @@ alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N'
 allow_doubles	= true 	# Allow the same letter twice in a row
 allow_triples	= false # Allow the same letter three times in a row
 
+bold=""
+red =""
+chanowner="---"
+botmaster="---"
+normal=""
+nick="AcroBot"
+# The builtin help system. This is a horrible kludge, but it works. :P
+help_main01	= bold + red + "How to play acro: " + normal + "At the start of each round I will give you an acro (for example: lmao). You then have to make up a phrase to fit that acro (for example: leather makes ankles oily)."
+help_main02	= "You enter your acro by typing /msg " + nick + " <your answer>"
+help_main03	= "After the alotted time has passed, I will display a numbered list of all the acros entered and a vote will then be held to determine the winner. You must then pick your favourite acro and vote for it."
+help_main04	= "You submit your vote by typing ctrl+k and messaging " + nick + " <your vote> (" + bold + "note:" + normal + " you only have to type the number of your vote, for example /msg " + nick + " 5)"
+help_main05	= "For more information, please ask " + chanowner + ", " + botmaster + " or one of the ops for help."
+# END CONFIGURATION
+
 
 class AcroBot():
-	def __init__(self, token):
+	def __init__(self):
 		print("AcroBot online!")
 
 		self.rnd = random.Random(time.time())
@@ -41,8 +56,9 @@ class AcroBot():
 		self.which_round = 0
 		self.acro = ""
 		self.mode = ""
-		self.channel = ''
+		self.message = discord.Message
 
+	def run(self, token):
 		client.run(token)
 
 	def disconnect(self):
@@ -56,29 +72,19 @@ class AcroBot():
 			else:
 				self.scores[self.this_round_nicks[n]] = self.this_round_scores[n]
 
-		yield from client.send_message(message.channel, 'Hye, you called?')
-
-		self.connection.privmsg(self.chan, "\x02\x0304Voting time is up!")
+		yield from client.send_message(self.message.channel, "Voting time is up!")
 		for n in range(len(self.this_round_nicks)):
 			if self.this_round_scores[n] > 0:
-				self.connection.privmsg(self.chan,
-				                        "\x1F\x0313" + self.this_round_nicks[
-					                        n] + "'s\x0F \x02\x0312reponse was \x0F\x0306'" +
-				                        self.this_round_acros[
-					                        n] + "'\x0F\x02\x0312 and got \x0F\x02\x1F\x0313" + str(
-					                        self.this_round_scores[
-						                        n]) + "\x0F\x02\x0312 votes!")
+				yield from client.send_message(self.message.channel, self.this_round_nicks[n] + "'s response was '" + self.this_round_acros[n] + "'and got " + str(self.this_round_scores[n]) + " votes!")
 
 		if self.which_round == rounds:
-			self.endgame()
+			yield from self.endgame()
 			return
 
-		self.connection.privmsg(self.chan, "\x02\x0312The score stands at:")
+		yield from client.send_message(self.message.channel, "The score stands at:")
 		for nick in self.scores.keys():
 			if self.scores[nick] > 0:
-				self.connection.privmsg(self.chan,
-				                        "\x02\x0313" + nick + "\x0312 with a score of \x0313" + str(
-					                        self.scores[nick]) + "\x0312!")
+				yield from client.send_message(self.message.channel, nick + " with a score of " + str(self.scores[nick]) + "!")
 
 		self.this_round_nicks = []
 		self.this_round_acros = []
@@ -88,20 +94,19 @@ class AcroBot():
 		self.which_round += 1
 		self.gen_acro(start_acro + self.which_round - 1)
 		self.mode = "ACRO"
-		self.connection.privmsg(self.chan, "\x0304\x1FRound " + str(
-			self.which_round) + "!\x0F \x0304The new acro is: \x02\x0303" + self.acro)
+		yield from client.send_message(self.message.channel, "Round " + str(self.which_round) + "! The new acro is: " + self.acro)
 		threading.Timer(acro_time, self.switch_mode).start()
 
 	def switch_mode(self):
 		self.mode = "VOTE"
-		self.connection.privmsg(self.chan,
-		                        "\x02\x0304Acro time is up! Please vote now on the following choices:")
+		yield from client.send_message(self.message.channel, "Acro time is up! Please vote now on the following choices:")
 		for a in range(len(self.this_round_acros)):
-			self.connection.privmsg(self.chan, "\x0306\x1F#" + str(
-				a) + "\x0F: \x02\x0312" + self.this_round_acros[a])
+			yield from client.send_message(self.message.channel, "#" + str(a) + ": " + self.this_round_acros[a])
+
 		threading.Timer(vote_time, self.round).start()
 
-	def startgame(self, message):
+	def startgame(self):
+
 		# The following (hopefully) fixes a bug that carries
 		# acros and scores across games
 		self.scores = {}
@@ -115,82 +120,68 @@ class AcroBot():
 		self.gen_acro(start_acro)
 		self.mode = "ACRO"
 
-		yield from client.send_message(message.channel, 'Starting a new game! Get Ready!')
-		yield from client.send_message(message.channel, 'Round ' + str(self.which_round) + '!')
-		yield from client.send_message(message.channel, 'The new acro is: ' + self.acro)
+		yield from client.send_message(self.message.channel, 'Starting a new game! Get Ready!')
+		yield from client.send_message(self.message.channel, 'Round ' + str(self.which_round) + '!')
+		yield from client.send_message(self.message.channel, 'The new acro is: ' + self.acro)
 
 		threading.Timer(acro_time, self.switch_mode).start()
 
 	def endgame(self):
-		msg = self.connection.privmsg
-		msg(self.chan, "\x02\x0304The game is over!")
-		msg(self.chan, "\x02\x0304Ending scores:")
+		yield from client.send_message(self.message.channel, "\x02\x0304The game is over!")
+		yield from client.send_message(self.message.channel, "\x02\x0304Ending scores:")
 		for nick in self.scores.keys():
-			msg(self.chan,
-			    "\x0313\x1F" + nick + "\x0F\x02\x0312 with a score of \x0F\x1F\x0313" + str(
-				    self.scores[nick]) + "\x0F\x02\x0312!")
+			yield from client.send_message(self.message.channel, nick + " with a score of " + str(self.scores[nick]) + "!")
 
-	def on_welcome(self, c, e):
-		c.join(self.chan)
-		c.privmsg(self.chan,
-		          "Hello! Try '/msg " + nick + " !help' for more info. Have fun! :) ")
+	def on_privmsg(self, channel, message):
+		if message.author.id == client.user.id:
+			return
 
-	def on_privmsg(self, c, e):
-		if string.split(e.arguments()[0])[0] == "!help":
-			c.privmsg(irclib.nm_to_n(e.source()), help_intro)
-			c.privmsg(irclib.nm_to_n(e.source()), help_main01)
-			c.privmsg(irclib.nm_to_n(e.source()), help_main02)
-			c.privmsg(irclib.nm_to_n(e.source()), help_main03)
-			c.privmsg(irclib.nm_to_n(e.source()), help_main04)
-			c.privmsg(irclib.nm_to_n(e.source()), help_main05)
-		elif string.split(e.arguments()[0])[0] == "!start":
-			self.startgame()
-		elif string.split(e.arguments()[0])[0] == "!rehash":
-			c.privmsg(self.chan, "Restarting...")
-			self.start()
-		elif string.split(e.arguments()[0])[0] == "!shutdown":
-			c.privmsg(self.chan, "Have fun, bye!")
+		print("On privmsg")
+
+		if message.content.startswith("!help"):
+			yield from client.send_message(channel, help_main01)
+			yield from client.send_message(channel, help_main02)
+			yield from client.send_message(channel, help_main03)
+			yield from client.send_message(channel, help_main04)
+			yield from client.send_message(channel, help_main05)
+
+		elif message.content.startswith("!start"):
+			yield from self.startgame()
+		elif message.content.startswith("!shutdown"):
+			yield from client.send_message(channel, "Have fun, bye!")
 			self.disconnect()
 		elif self.on == false:
-			c.privmsg(e.source(),
-			          "Sorry, no game is currently running. Try '!help'.")
-		elif self.mode == "ACRO":
-			if self.confirm_acro(string.split(e.arguments()[0])) == false:
-				c.notice(irclib.nm_to_n(e.source()),
-				         "That does not match the acro. Try again.")
+			yield from client.send_message(message.channel, "Sorry, no game is currently running. Try '!help'.")
+		elif self.mode == "ACRO" and message.channel.is_private:
+			if self.confirm_acro(message.content.split()) == false:
+				yield from client.send_message(message.channel, "That does not match the acro. Try again.")
 				return
 			else:
-				if irclib.nm_to_n(e.source()) in self.this_round_nicks:
-					c.notice(irclib.nm_to_n(e.source()),
-					         "You may not enter more than once.")
+				if message.author.id in self.this_round_nicks:
+					yield from client.send_message(message.channel, "You may not enter more than once.")
 					return
-				elif e.arguments()[0] not in self.this_round_acros:
-					c.notice(irclib.nm_to_n(e.source()),
-					         "You've been entered in this round.")
-					self.this_round_nicks.append(irclib.nm_to_n(e.source()))
-					self.this_round_acros.append(e.arguments()[0])
+				elif message.content not in self.this_round_acros:
+					yield from client.send_message(message.channel, "You've been entered in this round.")
+					self.this_round_nicks.append(message.author.id)
+					self.this_round_acros.append(message.content)
 					self.this_round_scores.append(0)
 					return
 				else:
-					c.notice(irclib.nm_to_n(e.source()),
-					         "That acro has already been entered. Please try again.")
+					yield from client.send_message(message.channel, "That acro has already been entered. Please try again.")
 					return
-		elif self.mode == "VOTE":
+		elif self.mode == "VOTE" and message.channel.is_private:
 			try:
-				vote = int(string.split(e.arguments()[0])[0])
-				if irclib.nm_to_n(e.source()) not in self.this_round_nicks:
-					c.notice(irclib.nm_to_n(e.source()),
-					         "You can't vote if you don't participate.")
+				vote = int(message.content.trim())
+				if message.author.id not in self.this_round_nicks:
+					yield from client.send_message(message.channel, "You can't vote if you don't participate.")
 					return
-				if irclib.nm_to_n(e.source()) == self.this_round_nicks[vote]:
-					c.notice(irclib.nm_to_n(e.source()),
-					         "You can't vote for yourself. Try again.")
+				if message.author.id == self.this_round_nicks[vote]:
+					yield from client.send_message(message.channel, "You can't vote for yourself. Try again.")
 					return
-				if irclib.nm_to_n(e.source()) not in self.voted:
+				if message.author.id not in self.voted:
 					self.this_round_scores[vote] += 1
-					self.voted.append(irclib.nm_to_n(e.source()))
-					c.notice(irclib.nm_to_n(e.source()),
-					         "Your vote has been counted.")
+					self.voted.append(message.author.id)
+					yield from client.send_message(message.channel, "Your vote has been counted.")
 					return
 			except ValueError:
 				pass
@@ -199,7 +190,7 @@ class AcroBot():
 		if len(input) != len(self.acro):
 			return false
 		for i in range(len(self.acro)):
-			if string.capitalize(input[i])[0] != self.acro[i]:
+			if input[i][0].capitalize() != self.acro[i]:
 				return false
 		return true
 
@@ -232,6 +223,9 @@ class AcroBot():
 		return alphabet[-1]
 
 
+
+acro = AcroBot()
+
 @client.event
 @asyncio.coroutine
 def on_ready():
@@ -241,7 +235,7 @@ def on_ready():
 @client.event
 @asyncio.coroutine
 def on_message(message):
-	if message.content.startswith('!acro'):
-		yield from client.send_message(message.channel, 'Hye, you called?')
+	acro.message = message
+	yield from acro.on_privmsg(message.channel, message)
 
-AcroBot('MjIzNjI1MzU2ODAyOTE2MzU0.CrOq7Q.FBqjuX16eUTz214axIpBt_gYduI')
+acro.run('MjIzNjI1MzU2ODAyOTE2MzU0.CrOq7Q.FBqjuX16eUTz214axIpBt_gYduI')
